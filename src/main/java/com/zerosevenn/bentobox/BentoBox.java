6df80a42cb0a1687e56ae4448c049d1dc9e21d9b
Guilherme <guilherme@Rafaels-MacBook-Pro.local>
2024-12-20 13:14:40 -0300

    initial commit

---
+++ src/main/java/com/zerosevenn/bentobox/BentoBox.java
@@ -0,0 +1,17 @@
+package com.zerosevenn.bentobox;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BentoBox extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
